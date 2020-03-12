1bbf38b38fd4521c44bc2564a1920875df9c6221
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2020-03-12 20:01:27 -0300

    Cria e altera  usuário usando Active Record em rota model

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,34 @@ Route::get('/', function () {
     $helloWorld = 'Hello World';
     return view('welcome', ['hello' => $helloWorld]);
 });
+
+Route::get('/model', function () {       
+    /*
+    $user = new \App\User(); // Active Record - criação de usuário
+
+    $user->name = 'Usuário Teste';
+    $user->email = 'email@hotmail.com';
+    $user->password = bcrypt('123456');
+
+    $user->save(); // salva usuário no banco de dados
+
+    return \App\User::all();
+    */
+    
+    /*
+    $user = \App\User::find(17); // Active Record - busca de usuário
+    
+    $user->name = 'Usuário Modificado';
+    $user->email = 'email@market.com';
+
+    $user->save(); // salva usuário no banco de dados
+   
+    return \App\User::all();
+    */
+    
+    /*
+    $products = \App\Product::all(); // select * from products
+
+    return $products;
+    */
+});
