6f393650178b22896787101910daae501ef499a8
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2020-03-22 17:07:39 -0300

    Corrige 'setence' para 'sentence'

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(\App\Product::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'description' => $faker->setence,
+        'description' => $faker->sentence,
         'body' => $faker->paragraph(5, true),
         'price' => $faker->randomFloat(2, 10),
         'slug' => $faker->slug,
