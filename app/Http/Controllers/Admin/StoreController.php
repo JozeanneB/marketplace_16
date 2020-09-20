464de4e873c3fcdcea8db11e86c8a45645eb1781
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2020-09-20 00:42:02 -0300

    Adiciona usuário logado na criação de nova loja
    Remove seleção de usuário do formulário de criação de loja.

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -27,7 +27,7 @@ class StoreController extends Controller
         
         $data = $request->all();
 
-        $user = \App\User::find($data['user']);
+        $user = auth()->user();
         $store = $user->store()->create($data); 
 
         flash('Loja Criada com Sucesso')->success();
