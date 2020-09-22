dce26c1b2307518209c9238bc8e057ce4fdf005f
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2020-09-22 00:21:45 -0300

    Adiciona middleware UserHasStore

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRequest;
 class StoreController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('user.has.store')->only(['create', 'store']);
+    }
+    
     public function index()
     {
         $store = auth()->user()->store;
@@ -15,11 +20,6 @@ class StoreController extends Controller
 
     public function create()
     {
-        if (auth()->user()->store()->count() > 0) {
-            flash('Você já possui uma loja.')->warning();
-            return redirect()->route('admin.stores.index');
-        }
-
         $users = \App\User::all(['id', 'name']);
 
         return view('admin.stores.create', compact('users'));
