5fee9f1981a12777ae54d8bf7fef38b32c26532a
Jozeanne Belomo <jozeanne.belomo@gmail.com>
2020-09-21 23:15:02 -0300

    Adiciona mensagem de validação em campos
    Em view create de store.

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -7,22 +7,46 @@
         
         <div class="form-group">
             <label>Nome Loja</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror">
+
+            @error('name')
+                <dir class="invalid-feedback">
+                    {{$message}}
+                </dir>
+            @enderror
         </div>
         
         <div class="form-group">
             <label>Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror">
+        
+            @error('description')
+                <dir class="invalid-feedback">
+                    {{$message}}
+                </dir>
+            @enderror
         </div>
         
         <div class="form-group">
             <label>Telefone</label>
-            <input type="text" name="phone" class="form-control">
+            <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror">
+        
+            @error('phone')
+                <dir class="invalid-feedback">
+                    {{$message}}
+                </dir>
+            @enderror
         </div>
         
         <div class="form-group">
             <label>Celular / Whatsapp</label>
-            <input type="text" name="mobile_phone" class="form-control">
+            <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror">
+        
+            @error('mobile_phone')
+                <dir class="invalid-feedback">
+                    {{$message}}
+                </dir>
+            @enderror
         </div>
         
         <div class="form-group">
